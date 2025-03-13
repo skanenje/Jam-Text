3b6a3a6f72ef2fe7f43cdd39011c740dcc68c252
skanenje <swapomuse@gmail.com>
2025-03-13 11:30:22 +0300

    feat: add the datatypes we need in hashing

--- internal/simhash/simhash.go
+++ internal/simhash/simhash.go
@@ -1,1 +1,29 @@
 package simhash
+
+
+const (
+	VectorDimensions = 128
+	NumHyperplanes   = 64
+)
+// SimHash represents a 64-bit fingerprint
+type SimHash uint64
+// PermutationTable implements locality-sensitive hashing for faster similarity search
+type PermutationTable struct {
+	permutations [][]int
+	bandSize     int
+	bands        int
+}
+// Vectorizer defines an interface for converting text to vectors
+type Vectorizer interface {
+	TextToVector(text string) []float64
+}
+
+// FrequencyVectorizer implements simple frequency-based vectorization
+type FrequencyVectorizer struct {
+	dimensions int
+}
+// NGramVectorizer implements n-gram based vectorization
+type NGramVectorizer struct {
+	dimensions int
+	ngramSize  int
+}
