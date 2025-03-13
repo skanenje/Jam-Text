4023ec48bb73bedd148ab29bc936b23ffc232816
jerootieno <otienojerome6@gmail.com>
2025-03-13 17:06:37 +0300

    func: chunk processing and indexing

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -3,20 +3,50 @@ package cli
 import (
 	"flag"
 	"fmt"
+	"jamtext/internal/chunk"
+	"jamtext/internal/index"
+	"jamtext/internal/simhash"
+	"log"
+	"os"
+	"time"
 )
 
 func Run(args []string) error {
 	fs := flag.NewFlagSet("textindex", flag.ExitOnError)
+
+	// TODO: Add more flags
+	verbose := fs.Bool("v", false, "Enable Verbose output")
+	logFile := fs.String("log", "", "Log file path(default: stderr)")
 	
 	// Basic commands
 	cmd := fs.String("cmd", "", "Command to run")
 	input := fs.String("i", "", "Input file path")
 	output := fs.String("o", "", "Output file path")
+	size := fs.Int("s", 4096, "Chunk size in bytes")
 
 	// Advanced commands to be added
+	overlapSize := fs.Int("overlap", 256, "Overlap size in bytes")
+	splitBoundary := fs.Bool("boundary", true, "Split on text boundaries")
+	boundaryChars := fs.String("boundary-chars", ".!?\n", "Text boundaries to split on")
+	maxChunkSize := fs.Int("max-size", 6144, "Maximum chunk size in bytes")
+	preserveNewlines := fs.Bool("preserve-nl", true, "Preserve newlines in chunks")
+	indexDir := fs.String("index-dir", "", "Directory to store index shards")
 	
 
 	fs.Parse(args[1:])
+
+	// Setup logger
+	var logger *log.Logger
+	if *logFile != "" {
+		f, err := os.OpenFile(*logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+		if err != nil {
+			return fmt.Errorf("error opening log file: %v", err)
+		}
+		defer f.Close()
+		logger = log.New(f, "", log.LstdFlags)
+	} else {
+		logger = log.New(os.Stderr, "", log.LstdFlags)
+	}
 
 	switch *cmd {
 	case "index":
@@ -24,8 +54,42 @@ func Run(args []string) error {
 			return fmt.Errorf("input and output file paths must be specified")
 		}
 
-		// TODO: Setup chunk options
+		hyperplanes := simhash.GenerateHyperplanes(simhash.VectorDimensions, simhash.NumHyperplanes)
 
+		// TODO: Setup chunk options
+		opts := chunk.ChunkOptions{
+			ChunkSize:        *size,
+			OverlapSize:      *overlapSize,
+			SplitOnBoundary:  *splitBoundary,
+			BoundaryChars:    *boundaryChars,
+			MaxChunkSize:     *maxChunkSize,
+			PreserveNewlines: *preserveNewlines,
+			Logger:           logger,
+			Verbose:          *verbose,
+		}
+
+		start := time.Now()
+
+		// TODO: Add a function to index a file
+		idx, err := chunk.ProcessFile(*input, opts, hyperplanes, *indexDir)
+		if err != nil {
+			return err
+		}
+
+		if err := index.Save(idx, *output); err != nil {
+			return err
+		}
+
+		// TODO: Add a function to get stats for output file
+		stats := idx.Stats()
+		fmt.Printf("Indexed %d unique hashes with %d total positions in %v\n", 
+		                    stats["unique_hashes"], 
+		                    stats["total_positions"], 
+		                    time.Since(start))
+		fmt.Printf("Created %d shards\n", stats["shards"])
+
+		return nil
+		
 	case "lookup":
 		// TODO: Add a function to lookup chunks
 
