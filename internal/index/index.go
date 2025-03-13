7c74dca783e98baa599a8aae6410f2523ef8863c
kevwasonga <kevinwasonga116@gmail.com>
2025-03-13 14:59:36 +0300

    feat: implement index with sharding support

--- internal/index/index.go
+++ internal/index/index.go
@@ -1,1 +1,33 @@
 package index
+
+import (
+	"sync"
+	"time"
+
+	"jamtext/internal/simhash"
+)
+
+// Maximum number of entries per shard
+const (
+	MaxShardSize = 100000
+)
+
+// IndexShard represents a portion of the index
+type IndexShard struct {
+	SimHashToPos map[simhash.SimHash][]int64
+	ShardID      int
+}
+
+// Index stores SimHash mappings with sharding support
+type Index struct {
+	SourceFile    string
+	ChunkSize     int
+	Shards        []*IndexShard
+	ActiveShard   int
+	Hyperplanes   [][]float64
+	CreationTime  time.Time
+	LSHTable      *simhash.PermutationTable
+	IndexDir      string
+	mu            sync.RWMutex
+	ShardFilename string
+}
