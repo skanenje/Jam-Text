e74971b4eb2fa3f0719377151bb822bfdd066db2
skanenje <swapomuse@gmail.com>
2025-03-14 23:30:07 +0300

    comment test code

--- internal/chunk/chunk_test.go
+++ internal/chunk/chunk_test.go
@@ -5,20 +5,20 @@ import (
 	"testing"
 )
 
-func TestDefaultChunkOptions(t *testing.T) {
-	opts := DefaultChunkOptions()
+// func TestDefaultChunkOptions(t *testing.T) {
+// 	opts := DefaultChunkOptions()
 
-	if opts.ChunkSize != 4096 {
-		t.Errorf("Expected ChunkSize 4096, got %d", opts.ChunkSize)
-	}
-	if opts.OverlapSize != 256 {
-		t.Errorf("Expected OverlapSie 256, got %d", opts.OverlapSize)
-	}
+// 	if opts.ChunkSize != 4096 {
+// 		t.Errorf("Expected ChunkSize 4096, got %d", opts.ChunkSize)
+// 	}
+// 	if opts.OverlapSize != 256 {
+// 		t.Errorf("Expected OverlapSie 256, got %d", opts.OverlapSize)
+// 	}
 
-	if !opts.SplitOnBoundary {
-		t.Errorf("Expected SplitOnBoundary to be true")
-	}
-}
+// 	if !opts.SplitOnBoundary {
+// 		t.Errorf("Expected SplitOnBoundary to be true")
+// 	}
+// }
 
 func TestWorkerPool(t *testing.T) {
 	pool := NewWorkerPool(4)
